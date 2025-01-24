e0d0e16fdcb37db1879bd162118dd9d69d3d29bc
Shivam <sm2552744@gmail.com>
2025-01-24 19:58:54 +0530

    knowReact;

--- src/Routing/MainPage.js
+++ src/Routing/MainPage.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Routes, Route, Switch } from "react-router-dom";
 import {Component1}  from './Page1';
 import {Component2} from './Page2';
 import {Component3} from './Page3';
-
+import PageNFound from "./PageNFnd";
 
 
 const MainPage = () => {
@@ -13,7 +13,7 @@ const MainPage = () => {
       <Router>
 
         <nav>
-          <Link to="/Page1" style={{ margin: "10px" }}>
+          <Link to="/" style={{ margin: "10px" }}>
             Page-1
           </Link>
           <Link to="/Page2" style={{ margin: "10px" }}>
@@ -22,13 +22,15 @@ const MainPage = () => {
           <Link to="/Page3" style={{ margin: "10px" }}>
             Page-3
           </Link>
+          <Link to="/Box">Dashboard</Link>
         </nav>
 
 
         <Routes>
-          <Route path="/Page1" element={<Component1 />} />
+          <Route path="/" exact={true} element={<Component1 />} />
           <Route path="/Page2" element={<Component2 />} />
           <Route path="/Page3" element={<Component3 />} />
+          <Route path="*" element={<PageNFound/>}/>
         </Routes>
       </Router>
     </div>
